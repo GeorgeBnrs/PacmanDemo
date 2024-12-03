b272a0a4603821c085847d002336f6cce4b2d798
Firewolf05 <firewolfgb@gmail.com>
2024-12-03 03:54:26 +0200

    Add project files.

---
+++ PacmanDemo/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace PacmanDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(2, 57);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1309, 684);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::PacmanDemo.Properties.Resources.pacman_open;
+            this.pictureBox2.Location = new System.Drawing.Point(76, 29);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(35, 35);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::PacmanDemo.Properties.Resources.cherry1;
+            this.pictureBox1.Location = new System.Drawing.Point(149, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(35, 35);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(2, -1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1309, 57);
+            this.panel2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(76, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 4000;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1312, 743);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PacmanDemo/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PacmanDemo
+{
+    public partial class Form1 : Form
+    {
+        int msec = 0;
+        string direction = "right";
+        Image originalImage = Properties.Resources.pacman_open;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+            this.KeyDown += Form1_KeyDown;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            msec++;
+            updateTimer(msec);
+            if (direction == "up")
+            {
+                pictureBox2.Location = new Point(pictureBox2.Location.X, pictureBox2.Location.Y - 10);
+
+            }
+            else if (direction == "down")
+            {
+                pictureBox2.Location = new Point(pictureBox2.Location.X, pictureBox2.Location.Y + 10);
+            }
+            else if (direction == "left")
+            {
+                pictureBox2.Location = new Point(pictureBox2.Location.X - 10, pictureBox2.Location.Y);
+            }
+            else if (direction == "right")
+            {
+                pictureBox2.Location = new Point(pictureBox2.Location.X + 10, pictureBox2.Location.Y);
+
+            }
+
+            if (pictureBox1.Bounds.IntersectsWith(pictureBox2.Bounds))
+            {
+                Random rnd = new Random();
+                int cherryX = rnd.Next(panel1.Width - 35); // -35 so it doesnt appear outside the panel sometimes, since the location is of the top left corner 
+                int cherryY = rnd.Next(panel1.Height - 35);
+                pictureBox1.Location = new Point(cherryX, cherryY);
+            }
+
+        }
+
+        private void updateTimer(int msec)
+        {
+            int mins = msec / 600;
+            int secs = msec / 10 - (mins * 60);
+            int msecs = msec - (mins * 600) - (secs * 10);
+            label1.Text = mins.ToString() + " : " + secs.ToString() + " : " + msecs.ToString();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            Image imgUp = originalImage;
+            imgUp.RotateFlip(RotateFlipType.Rotate270FlipNone);
+            Image imgDown = originalImage;
+            imgDown.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            Image imgLeft = originalImage;
+            imgLeft.RotateFlip(RotateFlipType.RotateNoneFlipX);
+
+            switch (e.KeyCode)
+            {
+                
+                case Keys.Up:
+                    label2.Text = "Up";
+                    direction = "up";
+                    pictureBox2.Image = imgUp;
+                    break;
+                case Keys.Down:
+                    label2.Text = "Down";
+                    direction = "down";
+                    pictureBox2.Image = imgDown;
+                    break;
+                case Keys.Left:
+                    label2.Text = "Left";
+                    direction = "left";
+                    pictureBox2.Image = imgLeft;
+                    break;
+                case Keys.Right:
+                    label2.Text = "Right";
+                    direction = "right";
+                    pictureBox2.Image = originalImage;
+                    break;
+            }
+        }
+    }
+}
+
